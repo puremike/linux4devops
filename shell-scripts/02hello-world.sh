f3a5b28e33f34cd9e6269c07eb7716bfa0e3ba61
puremike <michaelmarketing25@gmail.com>
2024-04-22 09:32:47 +0100

    Continuing my basic shell-scripting course

--- shell-scripts/02hello-world.sh
+++ shell-scripts/02hello-world.sh
@@ -6,3 +6,16 @@ echo "\${#} : ${#}"
 echo "\${@} : ${@}"
 echo "\${1} : ${1}"
 
+var1=10
+
+if [ $var1 == 10 ]; then
+    echo "The numbers match"
+else
+    echo "The numbers don't match"
+fi
+
+name1="Mike"
+name2="Egbin"
+occupation="DevOps Engineer"
+
+echo "My name is $name1 $name2 and I'm a $occupation."
--- shell-scripts/04loop.sh
+++ shell-scripts/04loop.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# for item in item1 item2 item3; do
+#     echo $item
+# done
+
+# for i in {1..10}; do #{1..10} - numbers ranging from 1 - 10
+#     echo "Counting ...$i"
+# done
+
+for ((i = 1; i <= 10; i++)); do
+    echo "Number $i"
+done
