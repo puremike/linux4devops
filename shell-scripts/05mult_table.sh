17c698371822105bb137031e9e97a590aea0116b
puremike <michaelmarketing25@gmail.com>
2024-04-26 08:59:40 +0100

    updated my 05mult_table.sh script

--- shell-scripts/05mult_table.sh
+++ shell-scripts/05mult_table.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-read -p "Enter a valid number between 1 - 10: " num
-read -p "Enter a starting range number valid between 1 - 10: " startNum
-read -p "Enter an ending range number valid between 1 - 10: " endNum
+#using list form for loop
+read -p "Enter a number for the multiplication table: " num
+read -p "Do you want a full table or a partial table? (Enter 'f' for full, 'p' for partial): " tab
+read -p "Enter the starting number (between 1 and 10): " startNum
+read -p "Enter the ending number  (between 1 and 10): " endNum
 
-if [ $startNum -gt $endNum ]; then
-    echo "Invalid Range of Numbers; Here is the full multiplication table for $num"
-    # using the list form
-    for ((i = 1; i <= 10; i++)); do
+if [ $endNum -ge $startNum ]; then
+    echo "The partial multiplication table for $num from $startNum to $endNum: "
+    for ((i = $startNum; i <= $endNum; i++)); do
         "$num x $i = "$((num * i))
     done
 
-elif [ $endNum -ge $startNum ]; then
-    # using the list form
-    for ((i = $startNum; i <= $endNum; i++)); do
+elif [ $startNum -gt $endNum ]; then
+    echo -e "Invalid range. Showing full table instead. \nThe full multiplication table for $num:"
+    for ((i = 1; i <= 10; i++)); do
         "$num x $i = "$((num * i))
     done
 
